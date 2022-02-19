da83c7e13d0d0da115120ed0eb0d55558f9a114f
NishaDeore123 <93429268+NishaDeore123@users.noreply.github.com>
2022-02-19 18:10:23 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DotNetExam.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductID { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required (ErrorMessage ="Please Enter Product Name")]
+        public string ProductName{ get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter Rate of Product")]
+        public decimal Rate { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter Product Description")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter Product Category")]
+        public string CategoryName { get; set; }
+    }
+}
